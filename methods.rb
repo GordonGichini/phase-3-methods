0e97ce7652d62995caaf84cf59fe6848249daf7d
GordonGichini <gordongichini1@gmail.com>
2022-08-18 05:39:58 +0300

    commit tree 519848cdd25522b90cb116329f448ec76e633581

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet (name = "Naureen")
+   puts "Hello, #{name}!\n" 
+end
+def greet (name = "Jimmy")
+    puts "Hello, #{name}!\n"
+end
+
+def greet_with_default (name = "Naureen")
+   puts "Hello, #{name}!\n"
+   puts "Hello, programmer!\n"
+end
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+def halve (x)
+    if x.class != Integer
+    return nil
+end
+x/2
+end
